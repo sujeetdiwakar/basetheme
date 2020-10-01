927e6cf7de6c28212739ffb6ab4c68c77870e4c0
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2020-10-01 07:48:40 +0530

    Add widget in footer file

--- basetheme/footer.php
+++ basetheme/footer.php
@@ -4,6 +4,11 @@
  */
 ?>
 	<footer class="footer bg-primary">
+		<?php if ( is_active_sidebar( 'footer_widgets' ) ): ?>
+			<div class="footer__widgets container">
+				<?php dynamic_sidebar( 'footer_widgets' ); ?>
+			</div>
+		<?php endif; ?>
 		<p class="text-center"><?php echo __( '©' ) . date( ' Y ' ) . __( 'All Rights Reserved.' ); ?>
 			<a style="color: #fff;" href="<?php echo get_option( 'home' ); ?>/"><?php bloginfo( 'name' ); ?>.</a>
 		</p>
